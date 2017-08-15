441d06e00438f0bb0cf1b19076f3fc9c34220e53
frodriguezAlianza <f_rodriguez@alianza.edu.uy>
2017-08-15 20:35:50 -0300

    Ejemplos

---
+++ 3/js/custom.js
@@ -0,0 +1,10 @@
+$('document').ready(function() {
+    
+	$('#slide4 h1').hover(function(){
+		$('#slide4 h1').addClass("agrandar");
+	});
+	$('#slide4 h1').mouseout(function(){
+		$('#slide4 h1').removeClass("agrandar");
+	});
+
+});
